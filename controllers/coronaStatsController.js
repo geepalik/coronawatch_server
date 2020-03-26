dd5123072eef96c8df9a1f597a4ac023f701cb0e
gilp <gil.palikaras@gmail.com>
2020-03-26 12:32:46 +0200

    return total data for each country

--- controllers/coronaStatsController.js
+++ controllers/coronaStatsController.js
@@ -4,7 +4,17 @@ exports.getStats = (req, res, next) => {
     CoronaStats
         .fetchAll()
         .then(stats => {
-            res.status(200).json({stats: stats});
+            const totalStats = [];
+            for(stat of stats) {
+                const lastStatsItem = stat['stats'].slice(-1)[0];
+                totalStats.push({
+                    "country" : stat.country,
+                    "confirmed" : lastStatsItem.confirmed,
+                    "deaths" : lastStatsItem.deaths,
+                    "recovered" : lastStatsItem.recovered
+                });
+            };
+            res.status(200).json({"total_stats":totalStats,"timeline_stats": stats});
         })
         .catch(err => {
             next(err);
